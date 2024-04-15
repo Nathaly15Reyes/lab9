6553daf3eb18baff54c7e00c289c9944c276466f
Nathaly_Reyes <Nathaly_Reyes@LAB4-16>
2024-04-15 09:37:47 -0600

    Agregar archivos de proyecto.

---
+++ Alumnos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab9
+{
+    public class Alumnos
+    {
+        public string DPI {get; set; }
+        public string Name { get; set; }
+        public string Direccion { get; set; }
+
+
+    }
+}
